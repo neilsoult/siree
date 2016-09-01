bde71acaa0c1c49d00b429999d81ec7b3571243e
Neil Soult <neilsoult@users.noreply.github.com>
2016-09-01 15:31:26 -0700

    Update app.js

--- app.js
+++ app.js
@@ -5,45 +5,44 @@ angular.module('main.controller', ['data.factory'])
 ctrl.$inject = ['$uibModal', 'Data'];
 function ctrl ($uibModal, Data) {
   
-  var vm = this;
-  Data.initView(vm);
-  vm.selectStyle = function (style) {
+    var vm = this;
+    Data.initView(vm);
+    vm.selectStyle = function (style) {
+        
+        Data.showColors(vm, style);
+    };
+      
+    vm.submit = function () {
+        
+        var selectedColors = Object.keys(vm.selectedColors).filter(function (color) {
+            return vm.selectedColors[color];
+        });
     
-    Data.showColors(vm, style);
-  };
-  
-  vm.submit = function () {
-    
-    var selectedColors = Object.keys(vm.selectedColors).filter(function (color) {
-      return vm.selectedColors[color];
-    });
-
-    $uibModal.open({
-      //template: '<div><div class="modal-header"><h1>MODAL</h1></div><div class="modal-body"><button ng-click="close()">CLOSE</button></div></div>',
-      templateUrl: 'modal.html',
-      controller: modalCtrl,
-      controllerAs: 'mvm',
-      resolve: {
-        data: function () {
-          
-          return {
-            selectedColors: selectedColors,
-            title: vm.selectedStyle.name
-          };
-        }
-      }
-    });
-  };
+        $uibModal.open({
+            templateUrl: 'modal.html',
+            controller: modalCtrl,
+            controllerAs: 'mvm',
+            resolve: {
+                data: function () {
+              
+                    return {
+                        selectedColors: selectedColors,
+                        title: vm.selectedStyle.name
+                    };
+                }
+            }
+        });
+    };
 }
 
 modalCtrl.$inject = ['$uibModalInstance', 'data'];
 function modalCtrl ($uibModalInstance, data) {
   
-  var mvm = this;
-  console.log(data);
-  Object.assign(mvm, data);
-  mvm.close = function () {
-    
-    $uibModalInstance.dismiss();
-  }
+    var mvm = this;
+    console.log(data);
+    Object.assign(mvm, data);
+    mvm.close = function () {
+        
+        $uibModalInstance.dismiss();
+    }
 }
